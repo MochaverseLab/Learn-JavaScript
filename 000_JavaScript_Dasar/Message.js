851879d7ff655c3b8f04bc2373d77bc57ad9a394
Atyla A. Al Harits <damsadam78@gmail.com>
2023-09-21 19:50:12 +0700

    rename folders

---
+++ 000_JavaScript_Dasar/Message.js
@@ -0,0 +1,13 @@
+/* 
+
+Hallo temen-temen semua, selamat datang di repository JavaScript Study Notes 
+pada folder ini dan seterusnya kita akan membahas Tutorial JavaScript Dasar
+pastikan temen-temen semua mengikuti semua alur folder repository ini secara urut 
+
+Mohon maaf jika ada kesalahan penulisan dan komentar penjelasan yang tidak tepat
+semua yang saya pelajari akan saya usahakan push ke repository ini
+akan saya usahakan update sesering mungkin
+
+Terima Kasih :)
+
+*/
