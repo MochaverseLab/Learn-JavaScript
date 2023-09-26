5a6e3160b03d5d4eb831ab2697281df312c18c67
Atyla A. Al Harits <damsadam78@gmail.com>
2023-09-26 08:09:46 +0700

    add javascript oop

---
+++ 020_JavaScript_OOP/Message.js
@@ -0,0 +1,14 @@
+/* 
+
+Hallo temen-temen semua, terimakasih sudah mengikuti tutorial JavaScript Dasar
+pada mulai dari folder ini dan selajutnya kita akan membahas JavaScript OOP
+nah, apa itu OOP
+
+OOP adalah singkatan dari Object Oriented Programming
+OOP ini adalah konsep dalam pemrograman yang berorientasi pada objek / merujuk pada objek
+contoh objek seperti Mobil, Motor, Laptop, dan lain-lain
+untuk lebih jelasnya kita bahas di folder berikutnya
+
+Terima Kasih :)
+
+*/
