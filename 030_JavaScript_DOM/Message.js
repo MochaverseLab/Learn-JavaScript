2bb4e857f2220d19e18964c140df0567b4b98f9e
Atyla A. Al Harits <damsadam78@gmail.com>
2023-10-03 08:14:03 +0700

    add JavaScript DOM

---
+++ 030_JavaScript_DOM/Message.js
@@ -0,0 +1,10 @@
+/* 
+
+Hallo temen-temen semua, terimakasih sudah mengikuti tutorial JavaScript OOP
+selanjutnya kita akan membahas JavaScript DOM
+
+DOM adalah singkatan dari Document Object Model
+dengan DOM kita bisa memanipulasi element-element di HTML
+dengan JavaScript DOM website kita jadi lebih interaktif 
+
+*/
