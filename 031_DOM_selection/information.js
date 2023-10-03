e5487742aecc84967eff337ecc82a49c309fdd86
Atyla A. Al Harits <damsadam78@gmail.com>
2023-10-04 02:07:10 +0700

    add DOM Selection

---
+++ 031_DOM_selection/information.js
@@ -0,0 +1,20 @@
+// saat kalian menggunakan method-method selection kalian pasti akan menemukan element, nodeList, dan HTMLCollection
+// masing-masing method akan mengembalikan bentuk output yang berbeda
+
+
+
+// apa itu element, nodeList, dan HTMLCollection?
+
+// element adalah struktur komponen dasar yang membentuk HTML.
+// nodeList adalah object yang mewakili kumpulan node atau element HTML. nodeList mencakup element HTML, teks dan node lainnya yang ada di dalam dokumen.
+// HTMLCollection adalah object yang mewakili element HTML. HTMLCollection hanya mencakup element HTML saja
+
+
+
+// method apa saja yang menghasilkan element, nodeList, dan HTMLCollection?
+
+// getElementById()  => menghasilkan element
+// getElementsByClassName()  => menghasilkan HTMLCollection
+// getElementsByTagName()  => menghasilkan HTMLCollection
+// querySelector()   => menghasilkan element
+// querySelectorAll()   => menghasilkan nodeList
