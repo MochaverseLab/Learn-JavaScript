c9a83f15606d8c0b947c1e30c88d424cc7c9a9d6
Atyla A. Al Harits <damsadam78@gmail.com>
2023-12-23 12:25:19 +0700

    add promise

--- 04_JavaScript_Async/003_promise/promise.js
+++ 04_JavaScript_Async/003_promise/promise.js
@@ -1,43 +1,21 @@
 // Promise adalah adalah object yang meawakili nilai yang belum tersedia, namun akan tersedia pada suatu waktu dimasa depan
 // promise dapat diibaratkan seperti janji, janji ini punya tiga kemungkinan yaitu ditepati, diingkari, dan tertunda
-// tiga kemungkinan tadi di wakili oleh callback function yaitu resolve (ditepati), reject (diingkari), dan pending (tertunda)
 
 /* <===============================================================================> */
 /* <===============================================================================> */
 
-// Fungsi yang mengembalikan Promise
-function fetchData(isSuccess) {
-    return new Promise((resolve, reject) => {
-      // Simulasi operasi asynchronous
-      setTimeout(() => {
-        if (isSuccess) {
-          resolve('Data berhasil diambil');
-        } else {
-          reject('Gagal mengambil data');
-        }
-      }, 2000); // Simulasi waktu yang dibutuhkan untuk operasi asynchronous
-    });
-  }
-  
-  // Menggunakan Promise dengan .then() dan .catch()
-  fetchData(true)
-    .then((result) => {
-      console.log('Resolve:', result);
-    })
-    .catch((error) => {
-      console.error('Reject:', error);
-    });
-  
-  // Contoh chaining dengan Promise
-  fetchData(false)
-    .then((result) => {
-      console.log('Resolve:', result); // Tidak akan terpanggil karena operasi gagal
-      return 'Data tambahan'; // Memberikan nilai yang akan di-pass ke .then() berikutnya
-    })
-    .then((additionalData) => {
-      console.log('Additional Data:', additionalData);
-    })
-    .catch((error) => {
-      console.error('Reject:', error); // Akan terpanggil karena operasi gagal
-    });
-  
+function activity(data){
+  return new Promise((resolve, reject) => {
+    if(data == 'Baca Buku'){
+      resolve('Kamu akan menjadi pintar');
+    }else{
+      reject('Kamu akan menjadi bodoh');
+    }
+  });
+}
+
+activity('Baca Buku').then((result) => {
+  console.log(result);
+}).catch(error => {
+  console.log(error);
+})
