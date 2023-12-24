fdfe04a63e7479a68a69ea39374a764740d15008
Atyla A. Al Harits <damsadam78@gmail.com>
2023-12-24 13:13:46 +0700

    completed JavaScript BOM

---
+++ 06_javaScript_BOM/000_pendahuluan/Message.js
@@ -0,0 +1,12 @@
+/*
+
+JavaScript BOM adalah kumpulan beberapa object yang memberikan akses interaksi dengan browser
+berbeda dengan JavaScript DOM yang digunakan untuk manipulasi document html, JavaScript BOM digunakan untuk browser
+
+yang termasuk kedalam JavaScript BOM adalah window, screen, location, history, navigator, dan cookies
+
+BOM tidak memiliki standar resmi, dan implementasinya dapat bervariasi antar browser.
+
+Materi diambil dari dokumentasi: w3schools.com
+
+*/
